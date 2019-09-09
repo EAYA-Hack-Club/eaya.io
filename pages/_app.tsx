1d54eb1e23a238d628093ec8670b32a97f4011e4
Ben Aubin <ben@bensites.com>
2019-09-09 13:14:25 -0500

    init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import App from 'next/app'
+import Head from "next/head";
+
+class MyApp extends App {
+  // Only uncomment this method if you have blocking data requirements for
+  // every single page in your application. This disables the ability to
+  // perform automatic static optimization, causing every page in your app to
+  // be server-side rendered.
+  //
+  // static async getInitialProps(appContext) {
+  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  //   const appProps = await App.getInitialProps(appContext);
+  //
+  //   return { ...appProps }
+  // }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css?family=Cabin:400,700&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <style dangerouslySetInnerHTML={{__html: `
+            body {
+              font-family: "Cabin", sans-serif;
+              background-color: rgb(206, 41, 60);
+              background-image: linear-gradient(#FB40FF, #563AE8);
+              background-size: 100vw 100vh;
+              background-attachment: fixed;
+
+              font-weight: 400;
+            }
+        `}}/>
+      </Head>
+      <Component {...pageProps} />
+    </>;
+  }
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,67 @@
+export function IndexPage(props: {}) {
+  return <div>
+    <div className="card">
+      <h1>eaya.io</h1>
+      <h3>The EA Young Academy TLD</h3>
+
+      <div style={{fontWeight: 700, paddingTop: 8}}>
+        Want one? Shoot an email to Ben Aubin.
+      </div>
+
+      <hr/>
+        
+      <p>
+        
+      </p>
+
+      <p>
+        Available to all EAYA Students, Faculty, classes and clubs.
+      </p>
+
+      <h4>Resources:</h4>
+
+      <ul>
+        <li>
+          <a href="https://github.com/EAYA-Hack-Club/club-website/">
+            Source Code
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <style jsx>{`
+      .card {
+        background: #F9F9FA;
+        color: #555;
+
+        border-radius: 7px;
+        box-sizing: border-box;
+
+        margin: 10vh auto;
+
+        width: 90vw;
+        max-width: 480px;
+
+        padding: 40px;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+      }
+      h1 {
+        color: #893DEF;
+        font-weight: 700;
+
+        margin: 4px 0;
+      }
+      h3 {
+        font-weight: 400;
+        margin: 4px 0;
+      }
+
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+    `}</style>
+  </div>;
+};
+
+export default IndexPage;
